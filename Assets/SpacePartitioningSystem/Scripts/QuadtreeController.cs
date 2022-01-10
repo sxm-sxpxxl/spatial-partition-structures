20dac183a6837440cfdd5f2ac600ec61c7e5515e
sxm-sxpxxl <alescha.pestov@yandex.ru>
2022-01-11 04:41:18 +0700

    Реализовано обнаружение объектов, входящих в заданную область.

--- Assets/SpacePartitioningSystem/Scripts/QuadtreeController.cs
+++ Assets/SpacePartitioningSystem/Scripts/QuadtreeController.cs
@@ -7,12 +7,30 @@ namespace SpacePartitioningSystem
     // todo: to Quadtree separated class
     public class QuadtreeController : MonoBehaviour
     {
-        [SerializeField] private Color boundaryColor = Color.green;
-        [SerializeField] private Bounds boundary = new Bounds(); 
-        [SerializeField] private Transform[] storedObjects;
+        [Header("Debug Info")]
+        [SerializeField] private Color queryBoundaryColor = Color.green;
+
+        [Header("Quadtree parameters")]
+        [SerializeField] private Bounds boundary = new Bounds();
+        [SerializeField, Range(1, 64)] private int threshold = 1;
+        [SerializeField, Range(1, 128)] private int maxDepth = 16;
+
+        [Space]
+        [SerializeField] private Bounds queryBoundary = new Bounds();
         
-        private const int Threshold = 1;
-        private const int MaxDepth = 4;
+        [Space]
+        [SerializeField] private Transform[] storedObjects;
+        [SerializeField] private bool isRandomObjectsAdded = false;
+        [SerializeField, Range(1, 50)] private int randomObjectsCount = 10;
+        
+        [Space]
+        [SerializeField] private Transform randomObjectPrefab;
+        [SerializeField] private Transform randomObjectsContainer;
+
+        [Space]
+        [SerializeField, Range(0f, 5f)] private float delay = 2f;
+
+        private List<Transform> quadtreeStoredObjects = new List<Transform>(capacity: 100);
 
         private Node<Transform> root;
         
@@ -23,10 +41,10 @@ namespace SpacePartitioningSystem
             public List<T> values;
             public Bounds boundary;
 
-            public Node(Bounds boundary)
+            public Node(Bounds boundary, int threshold)
             {
                 childrens = null;
-                values = new List<T>(capacity: Threshold);
+                values = new List<T>(capacity: threshold);
                 this.boundary = boundary;
             }
             
@@ -35,17 +53,24 @@ namespace SpacePartitioningSystem
 
         private void OnDrawGizmos()
         {
-            Gizmos.color = boundaryColor;
-
+            Gizmos.color = queryBoundaryColor;
+            Gizmos.DrawCube(queryBoundary.center, queryBoundary.size);
+            
             if (root == null || root.childrens == null)
             {
+                Gizmos.color = Color.green;
                 Gizmos.DrawWireCube(boundary.center, boundary.size);
                 return;
             }
             
             void DrawNestedBoundaries(Node<Transform> node)
             {
-                Gizmos.color = node.values.Count == 0 ? boundaryColor : Color.green;
+                Gizmos.color = node.values.Count == 0 
+                    ? Color.green
+                    : node.values.Count == threshold
+                        ? Color.red
+                        : Color.blue;
+
                 Gizmos.DrawWireCube(node.boundary.center, node.boundary.size);
 
                 if (node.childrens == null)
@@ -60,21 +85,37 @@ namespace SpacePartitioningSystem
             }
 
             DrawNestedBoundaries(root);
+
+            var queryObjects = Query(queryBoundary);
+
+            if (queryObjects != null)
+            {
+                Gizmos.color = Color.white;
+                for (int i = 0; i < queryObjects.Length; i++)
+                {
+                    Gizmos.DrawSphere(queryObjects[i].position, 0.1f);
+                }
+            }
         }
 
         private void Awake()
         {
-            root = new Node<Transform>(boundary);
+            root = new Node<Transform>(boundary, threshold);
             HideAllObjects();
         }
 
         private void Start()
         {
-            StartCoroutine(AddObjectsCoroutine());
+            if (isRandomObjectsAdded)
+            {
+                StartCoroutine(AddRandomObjectsCoroutine());
+            }
+            else
+            {
+                StartCoroutine(AddStoredObjectsCoroutine());   
+            }
         }
         
-        private const float DELAY = 2f;
-
         private void HideAllObjects()
         {
             for (int i = 0; i < storedObjects.Length; i++)
@@ -82,35 +123,67 @@ namespace SpacePartitioningSystem
                 storedObjects[i].gameObject.SetActive(false);
             }
         }
+
+        private IEnumerator AddRandomObjectsCoroutine()
+        {
+            for (int i = 0; i < randomObjectsCount; i++)
+            {
+                float randomX = Random.Range(boundary.min.x, boundary.max.x);
+                float randomY = Random.Range(boundary.min.y, boundary.max.y);
+
+                var instance = Instantiate(randomObjectPrefab, new Vector3(randomX, randomY), Quaternion.identity, randomObjectsContainer);
+                instance.name = instance.name + $" ({i})";
+                instance.gameObject.SetActive(false);
+                
+                Debug.Log($"Adding \'{instance.name}\' random object...");
+                yield return new WaitForSeconds(delay);
+                
+                TryAdd(instance);
+                quadtreeStoredObjects.Add(instance);
+                instance.gameObject.SetActive(true);
+                
+                Debug.Log($"Object \'{instance.name}\' was added!");
+                Debug.Log($"************************************");
+            }
+            
+            // StartCoroutine(RemoveObjectsCoroutine());
+        }
         
-        private IEnumerator AddObjectsCoroutine()
+        private IEnumerator AddStoredObjectsCoroutine()
         {
             for (int i = 0; i < storedObjects.Length; i++)
             {
                 var target = storedObjects[i];
-                Debug.Log($"Adding \'{target.name}\' object...");
-                yield return new WaitForSeconds(DELAY);
+                Debug.Log($"Adding \'{target.name}\' stored object...");
+                yield return new WaitForSeconds(delay);
                 
                 TryAdd(target);
+                quadtreeStoredObjects.Add(target);
+                
                 target.gameObject.SetActive(true);
                 Debug.Log($"Object \'{target.name}\' was added!");
+                Debug.Log($"************************************");
             }
             
-            StartCoroutine(RemoveObjectsCoroutine());
+            // StartCoroutine(RemoveObjectsCoroutine());
         }
 
         private IEnumerator RemoveObjectsCoroutine()
         {
-            for (int i = 0; i < storedObjects.Length; i++)
+            for (int i = 0; i < quadtreeStoredObjects.Count; i++)
             {
-                var removedObject = storedObjects[i];
+                var removedObject = quadtreeStoredObjects[i];
                 Debug.Log($"Removing \'{removedObject.name}\' object...");
-                yield return new WaitForSeconds(DELAY);
+                yield return new WaitForSeconds(delay);
                 
                 TryRemove(removedObject);
+                
                 removedObject.gameObject.SetActive(false);
                 Debug.Log($"Object \'{removedObject.name}\' was removed!");
+                Debug.Log($"************************************");
             }
+
+            quadtreeStoredObjects.Clear();
         }
 
         // todo: value to object rename
@@ -125,6 +198,45 @@ namespace SpacePartitioningSystem
             return TryRemove(value, root);
         }
 
+        public Transform[] Query(Bounds queryBoundary)
+        {
+            return Query(root, queryBoundary);
+        }
+
+        private Transform[] Query(Node<Transform> node, Bounds queryBoundary)
+        {
+            // todo: 100?
+            List<Transform> queryObjects = new List<Transform>(capacity: 100);
+            if (node.boundary.Intersects(queryBoundary))
+            {
+                if (node.IsLeaf())
+                {
+                    for (int i = 0; i < node.values.Count; i++)
+                    {
+                        if (queryBoundary.Contains(node.values[i].position))
+                        {
+                            queryObjects.Add(node.values[i]);
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < node.childrens.Length; i++)
+                    {
+                        Transform[] childQueryObjects = Query(node.childrens[i], queryBoundary);
+                        if (childQueryObjects == null)
+                        {
+                            continue;
+                        }
+                        
+                        queryObjects.AddRange(childQueryObjects);
+                    }
+                }
+            }
+            
+            return queryObjects.ToArray();
+        }
+
         private bool TryAdd(Transform value, Node<Transform> node, int depth)
         {
             if (node.boundary.Contains(value.position) == false)
@@ -135,7 +247,7 @@ namespace SpacePartitioningSystem
             if (node.IsLeaf())
             {
                 // todo: AND max depth condition statement
-                if (depth >= MaxDepth || node.values.Count < Threshold)
+                if (depth >= maxDepth || node.values.Count < threshold)
                 {
                     node.values.Add(value);
                 }
@@ -212,7 +324,7 @@ namespace SpacePartitioningSystem
             }
 
             int totalObjectsCount = 0;
-            List<Transform> transferedObjects = new List<Transform>(capacity: Threshold);
+            List<Transform> transferedObjects = new List<Transform>(capacity: threshold);
             
             for (int i = 0; i < node.childrens.Length; i++)
             {
@@ -223,7 +335,7 @@ namespace SpacePartitioningSystem
                 
                 totalObjectsCount += node.childrens[i].values.Count;
 
-                if (totalObjectsCount > Threshold)
+                if (totalObjectsCount > threshold)
                 {
                     return false;
                 }
@@ -249,7 +361,7 @@ namespace SpacePartitioningSystem
 
             for (int i = 0; i < quadrantsCount; i++)
             {
-                node.childrens[i] = new Node<Transform>(new Bounds(Vector3.zero, childSize)); 
+                node.childrens[i] = new Node<Transform>(new Bounds(Vector3.zero, childSize), threshold); 
             }
 
             node.childrens[0].boundary.center = new Vector3((parentBoundary.center + childExtents).x, (parentBoundary.center + childExtents).y);
